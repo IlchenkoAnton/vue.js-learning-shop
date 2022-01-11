696e9283481c3b8995125c5fea527ae74b8eb828
ailchenko <ailchenko@solit-clouds.ru>
2022-01-11 23:32:27 +0300

    feat: Добавлен заголовок

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
-    <router-view/>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="row">
+                    <div class="col">
+                        <Header></Header>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <router-view></router-view>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        Подвал
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     import { mapActions } from 'vuex';
+    import Header from './components/Header.vue';
 
     export default {
         name: 'App',
+        components: {
+            Header,
+        },
         methods: {
             ...mapActions('authorization', [
                 'login'
